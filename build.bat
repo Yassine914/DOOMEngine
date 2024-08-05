f23a4dfad3521438873ba4176d2cabc6af25d4ad
Yassine914 <yassine.shehab206@gmail.com>
2024-08-05 18:47:43 +0300

    started using cmake. build.bat still exists but broken due to linking errors.

--- build.bat
+++ build.bat
@@ -1,50 +1,80 @@
 @echo off
-rem build script for DOOM Engine
-
 SetLocal EnableDelayedExpansion
 
-set cppFilesPath=
+echo ----------------------------
+echo build script for DOOM Engine
+echo      by Yassin Shehab
+echo ----------------------------
+echo.
 
-pushd src
-for /r %%x in (*.cpp) do (
-    set filePath=%%~x
-    echo %filePath%
-    set file=
+mkdir bin
+mkdir obj
 
-    for /l %%i in (1, 1, 34) do (
-        set file=!filePath~i!
-        rem echo %file%
-    )
-    
-    set cppFilesPath=!cppFilesPath! !filePath:~2!
-)
-
-if defined cppFilesPath set cppFilesPath=!cppFiles:~1!
-popd
-
-rem dependencies needing compilation
-set thirdparty= ./thirdparty/stb_image.cpp
-rem ./thirdparty/glad.c
-set assembly=doomEngine
-
-set compiler flags=-g -Wvarargs -Wall -Werror
-set includeFlags=-I./include/ -I./thirdparty/include/
-set linkerFlags=-L./thirdparty/lib/ -lglfw3 -lopengl32 -lgdi32
+set assembly=DoomEngine
 
 set defines=-D_DEBUG
 
-echo building %assembly%
+set cppFiles=
 
-if "%~1"=="-t" (
-    if "%~2"=="-v" (
-        clang++ -ftime-report %cppFiles%  %thirdparty% %compilerFlags% -o ./bin/%assembly% %defines% %includeFlags% %linkerFlags% 2>&1 | awk "/User Time/" RS="\n\n\n" ORS="\n\n\n"
-    ) else (
-        clang++ -ftime-report %cppFiles%  %thirdparty% %compilerFlags% -o ./bin/%assembly% %defines% %includeFlags% %linkerFlags% 2>&1 | awk "/Clang front-end time report/" RS="\n\n" ORS="\n\n"
-    )
-) else if "%~1"=="-v" (
-    clang++ -v %cppFiles%  %thirdparty% %compilerFlags% -o ./bin/%assembly% %defines% %includeFlags% %linkerFlags%
-) else (
-    clang++ %cppFiles%  %thirdparty% %compilerFlags% -o ./bin/%assembly% %defines% %includeFlags% %linkerFlags%
+pushd src
+
+for /r %%f in (*.cpp) do (
+    set filepath=%%~f
+
+    set "cppFiles=!cppFiles! !filePath:~36!"
 )
 
-echo %assembly% building complete.
+if defined cppFiles set cppFiles=!cppFiles:~1!
+
+echo C++ files to compile =^>
+
+for %%i in (%cppFiles%) do (
+    rem trick to sleep for milliseconds
+    ping 127.0.0.1 -n 1 -w 100 > nul
+    echo %%i
+)
+echo ----------------------------
+echo.
+
+popd
+
+echo compiling source code...
+
+set compilerFlags=-g -Wvarargs -Wall -Werror
+set includeFlags=-I./thirdparty/include/
+
+clang++ -c %cppFiles%  %compilerFlags% %includeFlags% %defines%
+
+@REM echo ----------------------------
+echo.
+
+echo linking...
+
+set objFiles=
+
+for /r %%f in (*.o) do (
+    set filepath=%%~f
+
+    set "filepath=!filepath!"
+    set objFiles=!objFiles! !filepath:~36!
+)
+
+set staticLibs=-lglfw3
+set dynamicLibs=-limgui
+
+set linkerFlags=-Lbin/  -Wl,-rpath,.
+set linkingLibs=-lopengl32 -luser32 -lgdi32
+
+clang++ %objFiles% -o ./bin/%assembly%.exe %linkerFlags% %staticLibs% %dynamicLibs% %linkingLibs% -v
+
+exit
+@REM for /r %%f in (*.o) do (
+    @REM set filepath=%%~f
+    @REM set filename=!filepath:~36!
+    @REM echo !filepath! !filename!
+    @REM move !filepath! %cwd%/obj/!filename!
+@REM )
+
+@REM echo building %assembly% complete...
+
+@REM exit
